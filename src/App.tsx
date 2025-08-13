316bb1fb8b855ec1cde9701bc92491497c147847
Vince Quiles <vjquiles@msn.com>
2025-08-13 15:15:32 -0400

    Initial scaffolding of App.tsx with the components. Import statements will be finished once i finish the components. I'm trying to follow the inclass advice of making the parent first to see what data is needed for the parent component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
+import { useState } from 'react'
+import { Container } from 'react-bootstrap'
+import SearchForm 
+import ResultsDisplay 
+import MapDisplay 
 
 function App() {
-
+  const [ipData, setIpData] = useState(null)
 
   return (
     <>
-
+      <Container>
+        <SearchForm onSearch={setIpData} />
+        <ResultsDisplay data={ipData} />
+        <MapDisplay data={ipData} />
+      </Container>
     </>
   )
 }
