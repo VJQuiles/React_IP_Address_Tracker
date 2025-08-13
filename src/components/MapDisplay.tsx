6e79e550e04fd81aa5df947ac6574be4d0941227
Vince Quiles <vjquiles@msn.com>
2025-08-13 17:41:15 -0400

    The map is now rendering albeit weirdly. I took a loot at the components in the dev tools, the outline of whats it supposed to be looks right, my guess is maybe i need to apply some css styling to fix the position like the last one. Either way, solid progress, I aint complaining, especially after the last project

--- src/components/MapDisplay.tsx
+++ src/components/MapDisplay.tsx
@@ -1,16 +1,29 @@
-import { Card } from "react-bootstrap"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import type { IPData } from "../services/apiService"
 
 type MapDisplayProps = {
-    data: any
+    data: IPData | null
 }
 
 export default function MapDisplay({ data }: MapDisplayProps) {
+    const champDefault = data ? [data.lat, data.lng] : [39.900833, -75.1675]
+    const mapKey = import.meta.env.VITE_MAP_KEY
+
     return (
-        <Card>
-            <Card.Body>
-                {/* Leaflet tags will be used a little later */}
-                <div>Hello, Map</div>
-            </Card.Body>
-        </Card>
-    )
+        <div style={{ height: "500px", width: "100%" }}>
+            <MapContainer center={champDefault} zoom={10} scrollWheelZoom={true} style={{ height: "100%", width: "100%" }}>
+                <TileLayer
+                    url={`https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${mapKey}`}
+                    attribution='<a href="https://www.maptiler.com/">MapTiler</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                />
+                {data && (
+                    <Marker position={[data.lat, data.lng]}>
+                        <Popup>
+                            {data.ip}, {data.location}
+                        </Popup>
+                    </Marker>
+                )}
+            </MapContainer>
+        </div>
+    );
 }
