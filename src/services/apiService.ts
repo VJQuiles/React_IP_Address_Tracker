54adc2fece75ac4bd200332389ae8b52d7f40c26
Vince Quiles <vjquiles@msn.com>
2025-08-13 17:18:08 -0400

    Api call set up, set up and env folder to be able to hide my api key, which is a lot easier than having to play hide the api key in the commit like last time, and then leveraging dist folders to get a working product.

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,3 +1,42 @@
+import { validateInput, NetworkError, DataError } from "../util/errorHandler";
+
+export interface IPData {
+    ip: string
+    location: string
+    timezone: string
+    isp: string
+    lat: number
+    lng: number
+}
+
 export async function fetchIPData(query: string) {
-    // will handle this in a little
+    validateInput(query)
+
+    const apiKey = import.meta.env.VITE_API_KEY
+    if (!apiKey) {
+        throw new Error("You didn't do this right bub")
+    }
+
+    const url = `https://geo.ipify.org/api/v1?apiKey=${apiKey}&${isNaN(Number(query)) ? `domain=${query}` : `ipAddress=${query}`
+        }`
+
+
+    const response = await fetch(url)
+    if (!response.ok) {
+        throw new NetworkError(`The internet is messin up: ${response.status}`)
+    }
+
+    const data = await response.json()
+    if (!data || typeof data !== "object") {
+        throw new DataError("That's not what i was expecting")
+    }
+
+    return {
+        ip: data.ip,
+        location: `${data.location.city}, ${data.location.region}`,
+        timezone: `UTC ${data.location.timezone}`,
+        isp: data.isp,
+        lat: data.location.lat,
+        lng: data.location.lng,
+    }
 }
